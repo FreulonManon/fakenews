e8fd03f934ffdce3ab2da86376a76a767d3e7eb3
jeremien <jeremie.nuel@gmail.com>
2019-05-15 12:11:41 +0200

    correction

--- fkn.py
+++ fkn.py
@@ -3,6 +3,7 @@ import json
 import random
 import nltk
 import ssl
+import re
 
 
 from nltk.corpus import wordnet as wn
@@ -27,36 +28,45 @@ liste3=[]
 texte = nltk.word_tokenize("And now for something completely different") 
 result=nltk.pos_tag(texte)
 #for data in result: 
-	
+    
 
 print(result)
 
-
- 
 for data in articles:
     for key, value in data.items():
         if (key == 'title'):
-        	if isinstance(value, str):
-        		liste1.append(value)
-        	else:
-            	liste1.append("no data")
+            if isinstance(value, str):
+            	liste1.append(value)
+            else:
+                liste1.append("no data")
         elif(key=='content'):
-        	liste2.append(value)
+            if isinstance(value, str):
+                	liste2.append(value)
+            else:
+                liste2.append("no data")
         elif(key=='publishedAt'):
-        	liste3.append(value)
+            if isinstance(value, str):
+                	liste3.append(value)
+            else:
+                liste3.append("no data")
 
 
+for l in liste2:
+    print(nltk.word_tokenize(l))
+    """
+    prendre la chaine de liste2
+    tokenization + postag
+    reconstituer des titres d'articles aléatoirement
+    determinant un verbe un nom
+    """
         
  
 f = open('ARTICLES.txt','w') 
 for i in range (len(liste1)):
     if (' ' in liste1[i]):
-    	sourceliste = liste1[i].split(" ")
-    	source = sourceliste[len(sourceliste)-1]
-    	#print(liste2[i], type(liste2[i]))
-	    #data = "JOURNAL: "+source+"\n"+"TITRE: "+liste1[i]+"\n"+"CONTENU: "+liste2[i]+"\n"+"DATE: "+liste3[i]+"\n\n\n"
-	    #f.write(data)
+        sourceliste = liste1[i].split(" ")
+        source = sourceliste[len(sourceliste)-1]
+        data = "JOURNAL: "+source+"\n"+"TITRE: "+liste1[i]+"\n"+"CONTENU: "+liste2[i]+"\n"+"DATE: "+liste3[i]+"\n\n\n"
+        f.write(data)
 
-#f.close()
-
-#print(f)
+f.close()
